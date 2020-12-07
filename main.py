12c871e76151a613ef8fb0b28254a122425b5d0c
douglas04 <msantosbrioso@alu.ufc.br>
2020-12-07 19:10:29 +0000

    Passou em todos os testes

--- main.py
+++ main.py
@@ -4,7 +4,11 @@
 def main():
     """Função principal que será rodada quando o script for passado para o interpretador."""
     # COLOQUE SEU CÓDIGO AQUI
-
+H = float(input("Qual sua altura?"))
+HH = 72.7 * H - 58
+HM = 62.1 * H - 44.7
+print(f"Se você for homem o seu peso ideal é {HH}.")
+print(f"Se você for mulher o seu peso ideal é {HM}.")
 
 if __name__ == '__main__':
     main()
